afd618a4ca93c5d5c654399c8cd626cf8895d304
ricardj <43671359+ricardj@users.noreply.github.com>
2020-07-19 12:18:09 +0200

    New commit.

--- utilityFunctions.js
+++ utilityFunctions.js
@@ -10,3 +10,39 @@ function IncreaseCounter() {
   currentCounter++;
   counterText.textContent = currentCounter;
 }
+
+//Resources to learn
+/**
+ * https://www.w3schools.com/
+ * https://www.tutorialspoint.com/index.htm
+ * https://codepen.io/ (https://codepen.io/DPDesigns/pen/bGEmyOq)
+ */
+
+//CSS
+/**
+ * http://www.flexboxdefense.com/
+ * https://flexboxfroggy.com/#es
+ * https://flukeout.github.io/
+ */
+
+/*+
+Exercises
+
+1. Tres en raya
+2. Hacer una galeria de videos de YouTube
+3. Hacer algún tipo de página que use APIs (algún tipo de visualizador)
+
+*/
+
+//Key things to learn
+/**
+ * Git y Github
+ * Javascript, Css y Html
+ * Funcionamiento de Https (como hacer llamadas POST, GET. Que es AJAX. Que es JSON)
+ * Uso de API s externas (APIs de google, API de Youtube, APIs de chatbot, API de NBA, API de IMDB, API de Instagram,API de la NASA, API de Chuck Norris... (https://api.chucknorris.io/))
+ * Uso de Frameworks Javascript o CSS (vue, jquery,angular, react... )
+ * Automatización: Gulp o Grunt, SCSS o SASS
+ * ...
+ * Backend: stacks de desarrollo,server y demás tecnologías (php, python, javascript...)
+ *
+ */
